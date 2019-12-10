ac1a0d24f488b0acdeb1d77e2f5e5167112bbd01
wu <wu.yuyuan@hotmail.com>
2019-12-10 17:09:58 +0100

    classeFilter

--- src/main/java/org/operationDragon/imagefilter/App.java
+++ src/main/java/org/operationDragon/imagefilter/App.java
@@ -8,21 +8,8 @@ import java.io.File;
 public class App extends JavaCVHelper{
     public static void main(String[] args) {
 
-        File f = new File("dragon.jpeg");
-
-        Mat image = opencv_imgcodecs.imread(f.getAbsolutePath());
-        image = filterGrayscale(image);
-        File outputDir = new File("output");
-        File outputFile = new File(outputDir, "result.jpg");
-        opencv_imgcodecs.imwrite(outputFile.getAbsolutePath(), image);
-
-         f = new File("dragonmignon2.jpeg");
-
-         image = opencv_imgcodecs.imread(f.getAbsolutePath());
-        image = filterGrayscale(image);
-        outputDir = new File("output");
-        outputFile = new File(outputDir, "result2.jpg");
-        opencv_imgcodecs.imwrite(outputFile.getAbsolutePath(), image);
+        FilterBlackWrite bw = new FilterBlackWrite();
+        bw.process();
 
 
 
