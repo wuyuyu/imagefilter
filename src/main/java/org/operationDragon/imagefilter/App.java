7a400fe794f3f18179e92a4bcd1d0a968681e7cb
Romain <romain.dupuis@edu.itescia.fr>
2019-12-10 16:04:01 +0100

    Images ajoutées

--- src/main/java/org/operationDragon/imagefilter/App.java
+++ src/main/java/org/operationDragon/imagefilter/App.java
@@ -1,7 +1,19 @@
 package org.operationDragon.imagefilter;
 
+import org.bytedeco.opencv.global.opencv_imgcodecs;
+import org.opencv.core.Mat;
+
+import java.io.File;
+
 public class App {
     public static void main(String[] args) {
 
+        File f = new File("dragon.jpeg");
+        Mat image = opencv_imgcodecs.imread(f.getAbsolutePath());
+        image = filterBlur(image);
+​
+        File outputDir = new File("output");
+        File outputFile = new File(outputDir, "result.jpg");
+        opencv_imgcodecs.imwrite(outputFile.getAbsolutePath(), image);
     }
 }
---
+++ src/main/java/org/operationDragon/imagefilter/JavaCVHelper.java
@@ -0,0 +1,47 @@
+package org.operationDragon.imagefilter;
+
+import org.bytedeco.opencv.global.opencv_imgcodecs;
+import org.bytedeco.opencv.opencv_core.Mat;
+import org.bytedeco.opencv.opencv_core.Size;
+import org.opencv.core.CvType;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+
+import static org.bytedeco.opencv.global.opencv_imgproc.*;
+
+public class JavaCVHelper {
+
+    public void exempleBlurFile() {
+        File f = new File("imgs/test.jpg");
+        Mat image = opencv_imgcodecs.imread(f.getAbsolutePath());
+        image = filterBlur(image);
+
+        File outputDir = new File("output");
+        File outputFile = new File(outputDir, "result.jpg");
+        opencv_imgcodecs.imwrite(outputFile.getAbsolutePath(), image);
+    }
+
+    public Mat filterBlur(Mat image) {
+        int size = 3;
+        Mat result = image.clone();
+        GaussianBlur(image, result, new Size(size, size), 0);
+        return result;
+    }
+
+    public Mat filterDilate(Mat image) {
+        int size = 8;
+        Mat result = image.clone();
+        Mat element = getStructuringElement(Imgproc.MORPH_RECT, new Size(2 * size + 1, 2 * size + 1));
+        dilate(image, result, element);
+        return result;
+    }
+
+    public Mat filterGrayscale(Mat image) {
+        Mat result = new Mat(image.rows(), image.cols(), CvType.CV_8UC3);
+        cvtColor(image, result, Imgproc.COLOR_RGB2GRAY);
+        return result;
+    }
+
+
+}
